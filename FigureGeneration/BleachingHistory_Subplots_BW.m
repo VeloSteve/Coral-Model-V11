a3db1d9e01a450fb7531df03d56793a6c4151d3c
Steve Ryan <jaryan>
2017-09-06 14:39:53 -0700

    New Stressed-coral history plots
    These are all variations on a theme, for the various plot versions we have used.

--- FigureGeneration/BleachingHistory_Subplots_BW.m
+++ FigureGeneration/BleachingHistory_Subplots_BW.m
@@ -3,7 +3,7 @@ function BHSCO()
 %relPath = 'D:/CoralTest/V11Test_SC/bleaching/';
 relPath = 'D:\GoogleDrive\Coral_Model_Steve\_Paper Versions\Figures\Survival4Panel\bleaching_Target_5_SquaredOAFactor\';
 
-topNote = ''; %  {'5% Bleaching Target for 1985-2010', 'Original OA Factor CUBED'};
+topNote = ''; % {'5% Bleaching Target for 1985-2010', 'Original OA Factor CUBED'};
 
 figure('color', 'w');
 set(gcf, 'Units', 'inches', 'Position', [1, 1.5, 17, 11]);
@@ -13,19 +13,7 @@ set(gcf, 'Units', 'inches', 'Position', [1, 1.5, 17, 11]);
 rcpList = {'rcp26', 'rcp45', 'rcp60', 'rcp85'};
 rcpName = {'RCP 2.6', 'RCP 4.5', 'RCP 6.0', 'RCP 8.5'};
 
-%{
-    paperYears26 = [2000 2020 2028 2039 2040 2041 2044 2050 2060 2080 2099];
-    paperHFB26   = [0.5  4.0  11.0 31.0 30.0 29.0 27.0 17.0 18.0 23.0 17.0];
     
-    paperYears45 = [2000 2020 2028 2039 2040 2041 2044 2050 2058 2060 2070 2075 2080 2099];
-    paperHFB45   = [0.5  7.5  12.5 30.0 30.0 28.0 36.0 55.0 74.5 70.0 75.0 89.0 92.0 97.0];
-    
-    paperYears85 = [2000 2020 2028 2039 2040 2041 2044 2050 2060 2080];
-    paperHFB85   = [0.0  4.0  6.0  58.0 67.0 72.0 87.0 87.0 98.0 100.0];
-    paperYears60 = [2000 2020 2028 2039 2040 2041 2044 2050 2056 2060 2080 2099];
-    paperHFB60   = [0.5  2.5  7.0  25.0 30.0 33.0 42.0 46.0 51.0 61.0 98.0 100.0];
-%}
-
 legText = {};
 legCount = 1;
 for i = 1:4
@@ -46,21 +34,7 @@ for i = 1:4
     end
     % x values are the same for all. Use the latest file;
     load(hFile, 'xForPlot');
-    oneSubplot(xForPlot, cases, legText, rcpName{i}, i, i==1);
-
-    %{
-    if strcmp(rcpList{i}, 'rcp85')
-        oneSubplot(xForPlot, cases, legText, rcpName{i}, i, paperYears85, paperHFB85);
-    elseif strcmp(rcpList{i}, 'rcp60')
-        oneSubplot(xForPlot, cases, legText, rcpName{i}, i, paperYears60, paperHFB60);
-    elseif strcmp(rcpList{i}, 'rcp26')
-        oneSubplot(xForPlot, cases, legText, rcpName{i}, i, paperYears26, paperHFB26);    
-    elseif strcmp(rcpList{i}, 'rcp45')
-        oneSubplot(xForPlot, cases, legText, rcpName{i}, i, paperYears45, paperHFB45);
-    else
-        oneSubplot(xForPlot, cases, legText, rcpName{i}, i);
-    end
-    %}
+    oneSubplot(xForPlot, cases, legText, rcpName{i}, i==1);
     if i == 1
         yHandle = ylabel({'Percent of  ''damaged'' coral reefs globally'},'FontSize',22);
         set(yHandle, 'Position', [1925 -10 0])
@@ -84,25 +58,15 @@ if ~isempty(topNote)
 end
 end
 
-function oneSubplot(X, Yset, legText, tText, baseColor, useLegend) 
+function oneSubplot(X, Yset, legText, tText, showLeg) 
 
     % Create axes
     %axes1 = axes;
     %hold(axes1,'on');
-    switch baseColor
-        case 1 
-            base = [0 0 .9];
-            light = [0.5 0.5 1.0];
-        case 2 
-            base = [0 .7 0];
-            light = [0.4 1.0 0.4];
-        case 3 
-            base = [.6 .6 0];
-            light = [0.9 0.9 0];
-        case 4 
-            base = [.9 0 0];
-            light = [1.0 0.5 0.5];
-    end
+
+    base = [0 0 0];
+    light = [0.5 0.5 0.5];
+    
     col{1} = base;    % XXX - NOTE that this line is hidden for now.
     col{2} = base;
 
@@ -120,19 +84,8 @@ function oneSubplot(X, Yset, legText, tText, baseColor, useLegend)
             set(plot1(i), 'LineStyle', '--');
         end     
     end
-    % Old code - with extra x and y instead of useLegend
-    %{
-    if nargin == 7
-        % Add the Logan GCB line
-        hold on;
-        plot2 = plot(extraX, extraY);
-        set(plot2,...
-            'DisplayName','Logan et al. 2014', ...
-            'Color', [0 0 0], ...
-            'LineWidth', 2);
-        hold off;
-    end
-    %}
+
+
     % Create xlabel
     xlabel('Year','FontSize',13.2);
 
@@ -148,12 +101,8 @@ function oneSubplot(X, Yset, legText, tText, baseColor, useLegend)
     set(gca, 'FontSize',22,'XTick',[ 1950 2000 2050 2100 ],...
         'YTick',[0  50  100]);
     % Create legend
-    hold off;
-    if useLegend
+    if showLeg
         legend1 = legend('show');
         set(legend1,'Location','northwest','FontSize',20);
     end
-
-
-    
 end
